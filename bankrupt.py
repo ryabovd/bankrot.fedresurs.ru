d6020478a0eac3f39ae1ed6d07b56c13f10840d7
ryabovd <ryabovd@outlook.com>
2024-10-27 13:47:23 +0700

    Work on fio in old fedresurs, other data on fedresurs. Need clean comments.

--- bankrupt.py
+++ bankrupt.py
@@ -9,6 +9,7 @@ from urllib.parse import quote
 import pandas as pd
 import time
 import random
+from bs4 import BeautifulSoup
 
 
 kernel32 = ctypes.windll.kernel32
@@ -32,13 +33,15 @@ numbers = white_text_on_blue
 def build_url(prsnbankruptsId, regionId='95'):
     '''Build str url for parse'''
     start_url = "https://bankrot.fedresurs.ru/backend/prsnbankrupts?searchString="
-    middle_url = "&isActiveLegalCase=null&regionId=" # Это для всех дел независимо от статуса
+    middle_url = "&regionId=all&isActiveLegalCase=null"
+#    middle_url = "&isActiveLegalCase=null&regionId=" # Это для всех дел независимо от статуса
 #    middle_url = "&isActiveLegalCase=true&regionId=" # Это для активных дел
 #    middle_url = "&isActiveLegalCase=false&regionId=" # Это для завершенных дел
 
     end_url = "&limit=15&offset=0"
-    encoding_prsnbankruptsId = quote(prsnbankruptsId)
-    return start_url + encoding_prsnbankruptsId + middle_url + regionId + end_url
+    encoding_prsnbankruptsId = start_url + prsnbankruptsId + middle_url + end_url
+    print(start_url + prsnbankruptsId + middle_url + end_url)
+    return start_url + prsnbankruptsId + middle_url + end_url
 
 
 def get_prsnbankruptsId():
@@ -70,14 +73,172 @@ def get_debtors():
 
 def check_debtors(debtors):
     for debtor in debtors:
-        id = debtor.strip().lower()
-#        print('id', id)
-        print(f'Проверка {debtors.index(debtor) + 1} из {len(debtors)} - {debtor.strip()} - {(debtors.index(debtor) + 1) * 100 // len(debtors)} % завершено')
-        check_person(id)
-        asleep = random.randint(2000, 6000) / 1000
-#        print('sleep', asleep)
-        time.sleep(asleep)
-#        pass
+        prslastname, prsfirstname, prsmiddlename = debtor.lower().split()
+        print(f'Проверка {debtors.index(debtor) + 1} из {len(debtors)} - {(debtors.index(debtor) + 1) * 100 // len(debtors)}% завершено')
+        if get_old_response(prslastname, prsfirstname, prsmiddlename) != None:
+            prsn_name, prsn_inn = get_old_response(prslastname, prsfirstname, prsmiddlename)
+            print(prsn_name, type(prsn_name), prsn_inn, type(prsn_inn))
+            asleep = random.randint(2000, 5000) / 1000
+            time.sleep(asleep)
+            get_response(prsn_name, prsn_inn)
+        else:
+            pass
+
+
+def get_old_response(prslastname='', prsfirstname='', prsmiddlename='', regionid = '95'):
+    prslastname, prsfirstname, prsmiddlename = quote(prslastname), quote(prsfirstname), quote(prsmiddlename)
+    session = get_session()
+    url = 'https://old.bankrot.fedresurs.ru/DebtorsSearch.aspx/'
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'ru-RU,ru;q=0.9,en-RU;q=0.8,en;q=0.7,en-US;q=0.6',
+        'cache-control': 'no-cache',
+        'connection': 'keep-alive',
+        'cookie': '_ym_uid=1700107103592041594; _ym_d=1728816354; ASP.NET_SessionId=sljw4ny104xlpbsk54vb3wqh; _ym_isad=2; bankrotcookie=fac94585d97b923e5b4447836196b410; _ym_visorc=w; debtorsearch=typeofsearch=Persons&orgname=&orgaddress=&orgregionid=&orgogrn=&orginn=&orgokpo=&OrgCategory=' + '&prslastname=' + prslastname + '&prsfirstname=' + prsfirstname + '&prsmiddlename=' + prsmiddlename + '&prsaddress=&prsregionid=' + regionid + '&prsinn=&prsogrn=&prssnils=&PrsCategory=&pagenumber=0; qrator_msid=1728820063.842.5fEIfvU8SkJAvMc1-lscmto11bij4aoseoijabb61qkui5pr5',
+        'host': 'old.bankrot.fedresurs.ru',
+        'pragma': 'no-cache',
+        'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
+    }
+    data = session.get(url, headers=headers)
+    text = data.text
+    soup = BeautifulSoup(text, 'html.parser')
+    bank = soup.find('table', class_ = 'bank').find('tr').find_next_siblings('tr')
+    for el in bank:
+        prsn_data_list_dirty = str(el.get_text()).replace('\t', '').replace('Физическое лицо', '').split('\r\n')
+        prsn_data_list = clean_prsn_data(prsn_data_list_dirty)
+        person_old_link_end = get_person_old_link(soup)
+        person_old_link = build_person_old_link(person_old_link_end)
+        prsn_name, prsn_inn, prsn_snils, prsn_region, prsn_adress = parse_person_data(prsn_data_list)
+        print('NAME & INN', prsn_name, prsn_inn)
+        #prsn_fio, prsn_inn = get_debtor_fio_inn(person_old_link)
+        return prsn_name, prsn_inn
+
+
+def get_session():
+    s = requests.Session()
+    return s
+
+
+def clean_prsn_data(prsn_data_list):
+    clean_prsn_data_list = [i.strip() for i in prsn_data_list if i.strip()]
+    return clean_prsn_data_list
+
+
+def parse_person_data(prsn_data_list):
+    prsn_name, prsn_inn, prsn_snils, prsn_region, prsn_adress = prsn_data_list
+    return prsn_name, prsn_inn, prsn_snils, prsn_region, prsn_adress
+
+
+def get_fio(prsn_lastName, prsn_firstName, prsn_middleName):
+    fio = prsn_lastName + ' ' + prsn_firstName + ' ' + prsn_middleName
+    return fio
+
+
+def get_debtor_fio_inn(person_old_link):
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'ru-RU,ru;q=0.9,en-RU;q=0.8,en;q=0.7,en-US;q=0.6',
+        'cache-control': 'max-age=0',
+        'connection': 'keep-alive',
+        'cookie': '_ym_uid=1728029516311134247; _ym_d=1728029516; ASP.NET_SessionId=0bohgc1sylhu0kmbtchgkc11; debtorsearch=typeofsearch=Persons&orgname=&orgaddress=&orgregionid=&orgogrn=&orginn=1922547928&orgokpo=&OrgCategory=&prslastname=&prsfirstname=&prsmiddlename=&prsaddress=&prsregionid=&prsinn=&prsogrn=&prssnils=19225479288&PrsCategory=&pagenumber=0; _ym_isad=2; bankrotcookie=c98aae444770b29b2e0d2443407caf61; _ym_visorc=b; qrator_msid=1729866689.333.Ji8tvRKDjgadFiNb-ablknlgh28g2kh3rnbq6ifakmlou2kob',
+        'host': 'old.bankrot.fedresurs.ru',
+        'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
+    }
+    response = requests.get(person_old_link, headers=headers)
+    web_card = response.text
+    soup = BeautifulSoup(web_card, 'html.parser')
+    prsn_lastName = soup.find('span', id = 'ctl00_cphBody_lblLastName').text
+    prsn_firstName = soup.find('span', id = 'ctl00_cphBody_lblFirstName').text
+    prsn_middleName = soup.find('span', id = 'ctl00_cphBody_lblMiddleName').text
+    prsn_inn = soup.find('span', id = 'ctl00_cphBody_lblINN').text
+    prsn_fio = get_fio(prsn_lastName, prsn_firstName, prsn_middleName)
+    return(prsn_fio, prsn_inn)
+
+
+def get_debtor_old_card(person_old_link):
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'ru-RU,ru;q=0.9,en-RU;q=0.8,en;q=0.7,en-US;q=0.6',
+        'cache-control': 'max-age=0',
+        'connection': 'keep-alive',
+        'cookie': '_ym_uid=1728029516311134247; _ym_d=1728029516; ASP.NET_SessionId=0bohgc1sylhu0kmbtchgkc11; debtorsearch=typeofsearch=Persons&orgname=&orgaddress=&orgregionid=&orgogrn=&orginn=1922547928&orgokpo=&OrgCategory=&prslastname=&prsfirstname=&prsmiddlename=&prsaddress=&prsregionid=&prsinn=&prsogrn=&prssnils=19225479288&PrsCategory=&pagenumber=0; _ym_isad=2; bankrotcookie=c98aae444770b29b2e0d2443407caf61; _ym_visorc=b; qrator_msid=1729866689.333.Ji8tvRKDjgadFiNb-ablknlgh28g2kh3rnbq6ifakmlou2kob',
+        'host': 'old.bankrot.fedresurs.ru',
+        'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
+    }
+    response = requests.get(person_old_link, headers=headers)
+    web_card = response.text
+    #print(web_card)
+    soup = BeautifulSoup(web_card, 'html.parser')
+    prsn_lastName = soup.find('span', id = 'ctl00_cphBody_lblLastName').text
+#    print('prsn_lastName', prsn_lastName)
+    prsn_firstName = soup.find('span', id = 'ctl00_cphBody_lblFirstName').text
+#    print('prsn_firstName', prsn_firstName)
+    prsn_middleName = soup.find('span', id = 'ctl00_cphBody_lblMiddleName').text
+#    print('prsn_middleName', prsn_middleName)
+    prsn_birthdate = soup.find('span', id = 'ctl00_cphBody_lblBirthdate').text
+#    print('prsn_birthdate', prsn_birthdate)
+    prsn_birthplace = soup.find('span', id = 'ctl00_cphBody_lblBirthplace').text
+#    print('prsn_birthplace', prsn_birthplace)
+    prsn_caseRegion =  soup.find('span', id = 'ctl00_cphBody_lblRegion').text
+#    print('prsn_caseRegion', prsn_caseRegion)
+    prsn_inn = soup.find('span', id = 'ctl00_cphBody_lblINN').text
+#    print('prsn_inn', prsn_inn)
+    prsn_snils = soup.find('span', id = 'ctl00_cphBody_lblSNILS').text
+#    print('prsn_snils', prsn_snils)
+    prsn_address = soup.find('span', id = 'ctl00_cphBody_lblAddress').text
+#    print('prsn_address', prsn_address)
+    prsn_fio = get_fio(prsn_lastName, prsn_firstName, prsn_middleName)
+#    print('fio', prsn_fio)
+    out_card = prsn_fio, person_old_link, prsn_inn, prsn_snils, prsn_address
+    fill_out_card(prsn_fio, person_old_link, prsn_inn, prsn_snils, prsn_address)
+
+
+def fill_out_card(prsn_fio, person_old_link, prsn_inn, prsn_snils, prsn_address):
+    data['name'].append(prsn_fio)
+    data['snils'].append(prsn_snils)
+    data['inn'].append(prsn_inn)
+    data['address'].append(prsn_address)
+    data['link_old_fedresurs'].append(person_old_link)
+    #print('fill', data)
+
+
+def get_person_old_link(soup):
+    link = soup.find('table', class_ = 'bank').find('a')
+    person_link = link.get(('href'))
+    return(person_link)
+
+
+def build_person_old_link(person_old_link_end):
+    person_old_link_start = 'https://old.bankrot.fedresurs.ru'
+    person_old_link = person_old_link_start + person_old_link_end
+    return person_old_link
 
 
 def check_person(id):
@@ -96,14 +257,14 @@ data = {'name': [],
         'address': []}
 
 
-def get_response(id):
-    url = build_url(id)
+def get_response(prsn_name, prsn_inn):
+    url = build_url(prsn_inn)
     headers = {
         "Accept": "application/json, text/plain, */*",
         "Accept-Language": "ru-RU,ru;q=0.9,en-RU;q=0.8,en;q=0.7,en-US;q=0.6",
         "Connection": "keep-alive",
-        "Cookie": "_ym_uid=1700107103592041594; _ym_d=1716266926; qrator_msid=1723728014.226.j2YTgLfBJOAmPXHf-0tqah7dmflbeihg9s4gcea2gn707dkbq; _ym_isad=2; _ym_visorc=w",
-        "Referer": "https://bankrot.fedresurs.ru/bankrupts?searchString=^%^D0^%^BF^%^D0^%^B8^%^D1^%^81^%^D1^%^82^%^D1^%^83^%^D0^%^BD^%^D0^%^BE^%^D0^%^B2^%^D0^%^B8^%^D1^%^87^%^20^%^D1^%^81^%^D0^%^B5^%^D1^%^80^%^D0^%^B3^%^D0^%^B5^%^D0^%^B9&regionId=95&isActiveLegalCase=true&offset=0&limit=15^",
+        "Cookie": "_ym_uid=1728029516311134247; _ym_d=1728029516; _ym_isad=2; qrator_msid=1730009363.094.AsBwkpwu6WRUDT9z-mmi14i4b1deovhg89id1cq1bve2130e3; _ym_visorc=w",
+        "Referer": "https://bankrot.fedresurs.ru/bankrupts?searchString=190308294089",
         "Sec-Fetch-Dest": "empty",
         "Sec-Fetch-Mode": "cors",
         "Sec-Fetch-Site": "same-origin",
@@ -116,27 +277,27 @@ def get_response(id):
     response.encoding = 'utf-8'
     string = response.text
     res_dict = json.loads(string)
-#    print('res_dict', res_dict) # Печатаем полученный словарь
+    print('res_dict', res_dict) # Печатаем полученный словарь
+    print('total', res_dict['total'])
     if res_dict['total'] > 0:
-        print(red_text + str(id) + end_text)
+        print('ЗАШЁЛ')
+        print(red_text + str(prsn_name) + end_text)
         for dict in res_dict['pageData']:
-            if id == dict['fio'].lower():
-                print('fio', dict['fio'])
-                data['name'].append(dict['fio'])
-                if 'snils' in dict:
-                    print('snils', dict['snils'])
-                    data['snils'].append(dict['snils'])
-                else:
-                    print('snils', '0')
-                    data['snils'].append('0')
-                print('inn', dict['inn'])
-                data['inn'].append(dict['inn'])
-                data['case'].append(dict['lastLegalCase']['number'])
-                data['procedure'].append(dict['lastLegalCase']['status']['description'])
-                data['address'].append(dict['address'])
-                data['link_fedresurs'].append('https://fedresurs.ru/persons/' + dict['guid'] + ' ')
-    else:
-        pass
+            print('словарь из пэйдждата', dict)
+            data['name'].append(prsn_name)
+            if 'snils' in dict:
+                print('snils', dict['snils'])
+                data['snils'].append(dict['snils'])
+            else:
+                print('snils', '0')
+                data['snils'].append('0')
+            print('inn', dict['inn'])
+            data['inn'].append(dict['inn'])
+            data['case'].append(dict['lastLegalCase']['number'])
+            data['procedure'].append(dict['lastLegalCase']['status']['description'])
+            data['address'].append(dict['address'])
+            data['link_fedresurs'].append('https://fedresurs.ru/persons/' + dict['guid'] + ' ')
+            print('DATA', data)
 
 
 def start_time():
@@ -160,9 +321,8 @@ def date_today():
 def main():
     start = start_time()
     debtors = get_debtors()
-    check_debtors(debtors)
-#    print(data)
     today_date = str(date_today())
+    check_debtors(debtors)
     filename = 'bankrots_' + today_date + '.xlsx'
     if len(data['name']) > 0:
         print(red_text + 'Найдено ' + str(len(data['name'])) + ' записей' + end_text + "\n")
